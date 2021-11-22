6a01d87a830bd233236be530991c797ba754498f
Ewertton Silva <ewertton45mb@gmail.com>
2021-11-22 20:21:43 -0300

    First Commit Code D

---
+++ code_d.py
@@ -0,0 +1,17 @@
+# Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+
+# print("Hello World!")
+
+# frase = "Hello World!"
+# print(frase)
+
+# frase = input("Por favor, digite uma frase: ")
+# print(type(frase))
+# print(frase)
+
+# ---------------------------------------------------------------
+
+# Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+# num = int(input("Por favor, digite um número: "))
+# print(f"O número informado foi {num}")
